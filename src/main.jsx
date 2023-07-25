ea889dee2a659fc6621c051ef21efc74f765b4c4
ishnoopy <aldringuasa@gmail.com>
2023-07-25 21:31:16 +0800

    updated css url

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.scss'
+import '/src/App.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
