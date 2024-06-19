7f370d5c0b50a7cae0f890bc164d85f8a8dbd265
al <aldringuasa@gmail.com>
2024-06-20 02:12:28 +0800

    chore: replace CV url with the updated one

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const URL = {
   linkedin: 'https://www.linkedin.com/in/aldringuasa/',
   github: 'https://github.com/ishnoopy',
   email: 'mailto:aldringuasa@gmail.com',
-  CV: 'https://drive.google.com/file/d/1OI8_TpW8DNt7yCFb-75t6qAcs98QAdfF/view?usp=sharing'
+  CV: 'https://drive.google.com/file/d/1BWLa7FHfs1H8FnCeHRYw0tuAbUrbLd7t/view?usp=sharing'
 }
 
 
